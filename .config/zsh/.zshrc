c1a86f4686bd1df8c1fd8eb1951d1d2b6932c7a0
luck07051 <luck0751@gmail.com>
2022-06-28 07:43:45 +0800

    clear up nonecessary file

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -6,10 +6,10 @@ HISTFILE="$XDG_STATE_HOME"/zsh/history
 unsetopt autocd beep
 
 
-function zsh_add_file() { [ -f "$ZDOTDIR/$1" ] && source "$ZDOTDIR/$1" }
+function zsh_add_file() { [[ -f "$ZDOTDIR/$1" ]] && source "$ZDOTDIR/$1" }
 function zsh_add_plugin() { #{{{
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
+    if [[ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]]; then
         # For plugins
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,8 +1,34 @@
 
-export FZF_DEFAULT_OPTS="--height 40% --color=pointer:5,gutter:-1,prompt:7 --info=default"
+export FZF_DEFAULT_OPTS="--bind ctrl-j:down,ctrl-k:up \
+    --height 40% --color=pointer:5,gutter:-1,prompt:7 --info=default "
 export FZF_DEFAULT_COMMAND="fd --hidden \
     --ignore-file \"$ZDOTDIR\"/fzf-ignore --type file"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
+
+fd_path() {
+  fd --hidden --follow --exclude ".git" .
+}
+fd_dir() {
+  fd --type d --hidden --follow --max-depth 7 --exclude ".git" .
+}
+fzf_run() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)
+        target=`fd_dir | fzf "$@" --preview 'tree -C {} | head -200'`
+        [[ -n $target ]] && cd "$target"
+        ;;
+    vim)
+        vim `'ls' -a | fzf "$@" --preview 'cat {}'`
+        ;;
+
+    export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+    ssh)          fzf "$@" --preview 'dig {}' ;;
+    *)            fzf "$@" ;;
+  esac
+}
+
+bindkey -s '^F' 'fzf_run cd\n'
+bindkey -s '^V' 'fzf_run vim\n'
